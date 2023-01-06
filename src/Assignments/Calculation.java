4120e0b40da131ee792308df3d4f9b31ee304812
gadwa <srinivas>
2023-01-06 12:08:53 +0530

    New Changes

--- src/Assignments/Calculation.java
+++ src/Assignments/Calculation.java
@@ -10,6 +10,18 @@ public class Calculation {
 		c=z;
 	}
 	
+	public int sum(int x, int y) {
+		return (x+y);
+	}
+	public int sum(int x, int y, int z) {
+		return (x+y+z);
+	}
+	public double sum(double x, double y) {
+		return (x+y);
+	}
+	public double sum(double x, double y, double z) {
+		return (x+y+z);
+	}
 	public void sum() {
 		int add = 0;
 		add = a + b + c;
--- src/Assignments/CalculationMain.java
+++ src/Assignments/CalculationMain.java
@@ -6,6 +6,14 @@ public class CalculationMain {
 
 		Calculation cal = new Calculation(10, 20, 30);
 		cal.sum();
+		int a = cal.sum(10, 20);
+		double b = cal.sum(100, 200);
+		int c = cal.sum(10, 20, 30);
+		double d = cal.sum(100, 200, 300);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
 	}
 
 }
